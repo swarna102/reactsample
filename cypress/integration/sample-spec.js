3c3b60d424747211471583e9fcd8b664479ed128
swarna102 <yenugusana@gmail.com>
2021-07-11 00:00:15 +0530

    cypress pre-commit test

--- cypress/integration/sample-spec.js
+++ cypress/integration/sample-spec.js
@@ -1,5 +1,15 @@
 describe("my first test case", () => {
     it("Does not do match", () => {
-        expect(true).to.equal(false);
+        expect(true).to.equal(true);
+    });
+    it("visit site", () => {
+        cy.visit("/");
+        //cy.contains("swarna").click();
+        cy.contains("swarna").rightclick();
+        cy.url().should("include", "/");
+        cy.pause();
+        cy.debug();
+        cy.get("#root").find(".App");
+        cy.get("#root").find("a.App-link").should("contain", "swarna");
     });
 });
