711bf8cae90489b7ba973414dc601e89f07cf1da
swarna102 <yenugusana@gmail.com>
2021-07-11 20:41:22 +0530

    cypress pre-commit test sss sadfs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React swarna bangaram love you AAAAA
+                    Learn React swarna bangaram love you AAAAA AAA
                 </a>
             </header>
         </div>
